7736cbf1db3a555a769f302fc66301e3cb325586
peetzaecksdee <peetzaecksdee@skiff.com>
2024-04-02 12:22:29 +0700

    Insert Function

--- BST.js
+++ BST.js
@@ -12,39 +12,68 @@ class Tree {
 	}
 
 	buildTree(array) {
-    array = [...new Set(array)].sort((a, b) => a - b); // Remove Duplicates and sort
-		return this.BSTFromArray(array, 0, array.length - 1);
+		array = [...new Set(array)].sort((a, b) => a - b); // Remove Duplicates and sort
     
+		function recursiveBuild(start, end) {
+			if (start > end) return null;
+
+			let mid = Math.trunc((start + end) / 2);
+			let root = new Node(array[mid]);
+
+			root.left = recursiveBuild(start, mid - 1);
+			root.right = recursiveBuild(mid + 1, end);
+
+			return root;
+		}
+
+		return recursiveBuild(0, array.length - 1);
+
 		// for (let i = 1; i < array.length; i++) {
-      // 	let curr = this.root;
+		// 	let curr = this.root;
 		// 	while (curr) {
-      // 		if (array[i] < curr.val) {
+		// 		if (array[i] < curr.val) {
 		// 			if (!curr.left) {
 		// 				curr.left = new Node(array[i]);
-    //         break;
+		//         break;
 		// 			}
 		// 			curr = curr.left;
 		// 		} else {
-      // 			if (!curr.right) {
-        // 				curr.right = new Node(array[i]);
-        //         break;
+		// 			if (!curr.right) {
+		// 				curr.right = new Node(array[i]);
+		//         break;
 		// 			}
 		// 			curr = curr.right;
 		// 		}
 		// 	}
 		// }
 	}
-  
-  BSTFromArray(array, start, end) {
-    if (start > end) return null;
 
-    let mid = Math.trunc((start + end) / 2);
-    let root = new Node(array[mid]);
+	insert(val) {
 
-    root.left = this.BSTFromArray(array, start, mid - 1);
-    root.right = this.BSTFromArray(array, mid + 1, end);
+		function recursiveInsert(root) {
+			if (!root) return new Node(val);
 
-    return root;
+			if (root.val > val) {
+				root.left = recursiveInsert(root.left, val);
+			} else {
+				root.right = recursiveInsert(root.right, val);
+			}
+			return root;
+		}
+
+		this.root = recursiveInsert(this.root, val);
+	}
+
+  delete(val) {
+
+    function recursive(root) {
+      if (root.val === val) {
+        root
+        return;
+      }
+    }
+
+    recursive()
   }
 }
 
@@ -62,4 +91,7 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 };
 
 const newTree = new Tree([7, 1, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-prettyPrint(newTree.root)
+prettyPrint(newTree.root);
+newTree.insert(15);
+newTree.insert(12);
+prettyPrint(newTree.root);
